3feea6776e52f86f45bdfbcf0aefc4cfdcaa1408
Aron <ampdolar12204@gmail.com>
2026-06-02 17:21:37 +0800

    Fix duplicate projects user_id foreign key migration

--- database/migrations/2026_05_31_182014_update_user_id_in_projects_table.php
+++ database/migrations/2026_05_31_182014_update_user_id_in_projects_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id');
+            
         });
     }
 
