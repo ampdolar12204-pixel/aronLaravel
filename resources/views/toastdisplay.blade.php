42c84c6e61661e47148becd8c333577569851af9
Aron <ampdolar12204@gmail.com>
2026-06-02 17:38:20 +0800

    Update toastdisplay view

--- resources/views/toastdisplay.blade.php
+++ resources/views/toastdisplay.blade.php
@@ -2,7 +2,7 @@
     <div class="toast-container position-fixed top-0 end-0">
         <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
-                <img src="https://cdn-icons-png.freepik.com/512/9255/9255700.png" class="rounded me-2" alt="...">
+                <img src="https://cdn-icons-png.freepik.com/512/9255/9255700.png" height="10" width="10"  class="rounded me-2" alt="...">
                 <strong class="me-auto">Validation</strong>
                 <small>Now</small>
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
@@ -18,7 +18,7 @@
     <div class="toast-container position-fixed top-0 end-0">
         <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
-                <img src="https://cdn-icons-png.freepik.com/512/9255/9255700.png" class="rounded me-2" alt="...">
+                <img src="https://cdn-icons-png.freepik.com/512/9255/9255700.png" height="10" width="10" class="rounded me-2" alt="...">
                 <strong class="me-auto">Registration</strong>
                 <small>Now</small>
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
@@ -34,7 +34,7 @@
     <div class="toast-container position-fixed top-0 end-0">
         <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
-                <img src="https://cdn-icons-png.freepik.com/512/9255/9255700.png" class="rounded me-2" alt="...">
+                <img src="https://cdn-icons-png.freepik.com/512/9255/9255700.png" height="10" width="10"  class="rounded me-2" alt="...">
                 <strong class="me-auto">Registration</strong>
                 <small>Now</small>
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
